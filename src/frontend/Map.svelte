fa360434346b979d8b5722082117f2188f9827c4
HatimBenallal <hatim.benallal@estudiantat.upc.edu>
2023-05-19 17:29:53 +0200

    mapa

--- src/frontend/Map.svelte
+++ src/frontend/Map.svelte
@@ -66,16 +66,7 @@
 	  width: max-content;
 	}
   
-	#legend {
-	  bottom: 20px;
-	  background-color: #222222;
-  
-	  padding: 10px;
-	  border-radius: 10px;
-	  right: 10px;
-  
-	  width: max-content;
-	}
+	
   
 	#planes {
 	  left: 10px;
@@ -150,16 +141,13 @@
   <script lang="ts" type="module">
 	import { initializeMap } from "./arcgis/map";
   
-	import { fade } from "svelte/transition";
 	import type { Cat10 } from "./models/cat10";
 	import type { Cat21 } from "./models/cat21";
 	import { ipcMainBi } from "./ipcMain";
 
 	import Simulation from "./simulation.svelte";
 	import PlanesComponent from "./planesComponent.svelte";
-	import { setSMRVisibility, setADSBVisibility, setMLATVisibility } from "./arcgis/groundLayer";
-	import { setPlanesLayerVisibility, setPathsLayerVisibility } from "./arcgis/graphicsLayer";
-  
+	  
 	let messages: (Cat10 | Cat21)[] = [];
   
 	
@@ -169,12 +157,7 @@
 	let loading = false;
   
 	let settings = false;
-	let btncheckSMR = true;
-	let btncheckMLAT = true;
-	let btncheckADSB = true;
-	let btncheckAreas = true;
-	let btncheckPlanes = true;
-	let btncheckPaths = true;
+	
   
 	let performanceData = false;
   
@@ -257,94 +240,7 @@
 		</ul>
 	  {/if}
 	  {#if visibleItem === "MAP"}
-		{#if settings}
-		  <div class="ontop dark" id="settings" transition:fade="{{ duration: 100 }}">
-			<p>Visible Layers</p>
-  
-			<div role="group" aria-label="Basic checkbox toggle button group">
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckSMR}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setSMRVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckSMR"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckSMR">SMR</label>
-  
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckMLAT}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setMLATVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckMLAT"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckMLAT">MLAT</label>
-  
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckADSB}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setADSBVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckADSB"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckADSB">ADSB</label>
-			</div>
-			<div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-			  <!-- svelte-ignore missing-declaration -->
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckAreas}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setAreasLayerVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckAreas"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckAreas">MLAT Areas</label>
-			</div>
-			<div role="group" aria-label="Basic checkbox toggle button group">
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckPlanes}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setPlanesLayerVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckPlanes"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckPlanes">3D Planes</label>
-  
-			  <input
-				type="checkbox"
-				bind:checked="{btncheckPaths}"
-				on:change="{(e) => {
-				  //@ts-ignore
-				  setPathsLayerVisibility(e.target.checked);
-				}}"
-				class="btn-check"
-				id="btncheckPaths"
-				autocomplete="off"
-			  />
-			  <label class="btn btn-outline-primary" for="btncheckPaths">3D Paths</label>
-			</div>
-		  </div>
-		{/if}
+		
   
 		
 		<div class="ontop dark" id="btn-bar">
